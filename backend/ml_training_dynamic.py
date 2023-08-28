7eb0984fb8d687ea5e144e7772a9544a8981c0be
Alex Xu <alex.xu.205@gmail.com>
2023-08-28 16:13:26 -0400

    add react frontend, flask backend

--- backend/ml_training_dynamic.py
+++ backend/ml_training_dynamic.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import sys
 import lightgbm as lgb
@@ -6,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("dynamicfightstats.csv")
+data = pd.read_csv("backend/dynamicfightstats.csv")
 data.replace("--", pd.NA, inplace=True)
 
 selected_columns = [
@@ -62,7 +63,7 @@ pd.set_option("display.max_columns", None)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.2f}")
 
-predict_data = pd.read_csv("predictFights.csv")
+predict_data = pd.read_csv("backend/predictFights.csv")
 predict_data.replace("--", pd.NA, inplace=True)
 
 predict_data.dropna(subset=selected_columns, inplace=True)
@@ -85,6 +86,16 @@ for i, label in enumerate(label_encoder.classes_):
 
 print(predict_data)
 
+# Create a dictionary to store the predicted data and probabilities
+predicted_data_dict = {
+    "predict_data": predict_data.to_dict(orient="records"),
+    "class_probabilities": class_probabilities.tolist(),
+}
+
+# Save the dictionary as a JSON file
+with open("backend/predicted_data.json", "w") as json_file:
+    json.dump(predicted_data_dict, json_file)
+
 feature_importances = model.feature_importances_
 
 feature_importance_df = pd.DataFrame(
@@ -93,9 +104,9 @@ feature_importance_df = pd.DataFrame(
 
 feature_importance_df = feature_importance_df.sort_values("Importance", ascending=False)
 
-plt.figure(figsize=(10, 6))
-plt.barh(feature_importance_df["Feature"], feature_importance_df["Importance"])
-plt.xlabel("Importance")
-plt.ylabel("Feature")
-plt.title("Feature Importance")
-plt.show()
+# plt.figure(figsize=(10, 6))
+# plt.barh(feature_importance_df["Feature"], feature_importance_df["Importance"])
+# plt.xlabel("Importance")
+# plt.ylabel("Feature")
+# plt.title("Feature Importance")
+# plt.show()
