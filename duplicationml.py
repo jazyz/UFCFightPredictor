36a0121d5d544712f995a94356d5bad05b45af90
jazyz <johnlikeapple37@gmail.com>
2023-08-31 16:47:27 -0400

    testing

--- duplicationml.py
+++ duplicationml.py
@@ -14,10 +14,10 @@ selected_columns = [
     "fighter_str_differential",
     "fighter_td_differential",
     "fighter_sub_differential",
-    "fighter_winrate",
-    "fighter_winstreak",
+    # "fighter_winrate",
+    # "fighter_winstreak",
     "fighter_totalfights",
-    "fighter_totalwins",
+    # "fighter_totalwins",
     "fighter_titlefights",
     "fighter_titlewins",
     "fighter_elo",
@@ -25,10 +25,10 @@ selected_columns = [
     "opponent_str_differential",
     "opponent_td_differential",
     "opponent_sub_differential",
-    "opponent_winrate",
-    "opponent_winstreak",
+    # "opponent_winrate",
+    # "opponent_winstreak",
     "opponent_totalfights",
-    "opponent_totalwins",
+    # "opponent_totalwins",
     "opponent_titlefights",
     "opponent_titlewins",
     "opponent_elo",
@@ -58,12 +58,81 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# # ... (your previous code)
+
+fighter_train = X_train.copy()
+
+# Rename columns
+for col in fighter_train.columns:
+    fighter_train.rename(columns={col: col.replace("fighter_", "tmp_")}, inplace=True)
+
+for col in fighter_train.columns:
+    fighter_train.rename(columns={col: col.replace("opponent_", "fighter_")}, inplace=True)
+
+for col in fighter_train.columns:
+    fighter_train.rename(columns={col: col.replace("tmp_", "opponent_")}, inplace=True)
+
+# Reset index
+fighter_train.reset_index(drop=True, inplace=True)
+
+# Manual concatenation
+combined_X_train = pd.DataFrame()
+
+# Make sure column names match after renaming
+for col in fighter_train.columns:
+    combined_X_train[col] = pd.concat([fighter_train[col], X_train[col]], ignore_index=True)
+
+# Duplicate and flip labels
+combined_y_train = y_train._append(y_train, ignore_index=True)
+
+num_original_rows = len(y_train)
+num_duplicated_rows = len(combined_y_train) - num_original_rows
+
+for i in range(num_original_rows, len(combined_y_train)):
+    if combined_y_train[i] == "win":
+        combined_y_train[i] = "loss"
+    elif combined_y_train[i] == "loss":
+        combined_y_train[i] = "win"
+
+# ... (the rest of your code)
+
 model = lgb.LGBMClassifier(random_state=42)
-model.fit(X_train, y_train)
+model.fit(combined_X_train, combined_y_train)
 
-y_pred = model.predict(X_test)
+fighter_test = X_test.copy()
 
-accuracy = accuracy_score(y_test, y_pred)
+for col in fighter_test.columns:
+    fighter_test.rename(columns={col: col.replace("fighter_", "tmp_")}, inplace=True)
+
+for col in fighter_test.columns:
+    fighter_test.rename(columns={col: col.replace("opponent_", "fighter_")}, inplace=True)
+
+for col in fighter_test.columns:
+    fighter_test.rename(columns={col: col.replace("tmp_", "opponent_")}, inplace=True)
+
+# Reset index
+fighter_test.reset_index(drop=True, inplace=True)
+
+# Manual concatenation for test set
+combined_X_test = pd.DataFrame()
+
+# Make sure column names match after renaming
+for col in fighter_test.columns:
+    combined_X_test[col] = pd.concat([fighter_test[col], X_test[col]], ignore_index=True)
+combined_y_test = y_test._append(y_test, ignore_index=True)
+
+num_original_test_rows = len(y_test)
+num_duplicated_test_rows = len(combined_y_test) - num_original_test_rows
+
+for i in range(num_original_test_rows, len(combined_y_test)):
+    if combined_y_test[i] == "win":
+        combined_y_test[i] = "loss"
+    elif combined_y_test[i] == "loss":
+        combined_y_test[i] = "win"
+
+y_pred = model.predict(combined_X_test)
+
+accuracy = accuracy_score(combined_y_test, y_pred)
 print(f"Accuracy: {accuracy:.4f}")
 
 output_file = open("ml_elo.txt", "w")
