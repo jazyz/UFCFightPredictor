aa7a15c1faa5fad99c504199d75c6a75888886af
jazyz <johnlikeapple37@gmail.com>
2023-09-12 17:57:13 -0400

    made predicting fights easier

--- predict_fights_elo.py
+++ predict_fights_elo.py
@@ -32,55 +32,61 @@ def extract_fighter_stats(
         else:
             combined_stats["opponent_" + key] = value
     combined_stats["result"]="unknown"
-    with open(output_csv_filename, mode="w", newline="") as output_file:
-        fieldnames = [
-            "fighter_name",
-            "fighter_kd_differential",
-            "fighter_str_differential",
-            "fighter_td_differential",
-            "fighter_sub_differential",
-            "fighter_winrate",
-            "fighter_winstreak",
-            "fighter_losestreak",
-            "fighter_totalwins",
-            "fighter_totalfights",
-            "fighter_titlefights",
-            "fighter_titlewins",
-            "fighter_age_deviation",
-            "fighter_opp_avg_elo",
-            "fighter_elo",
-            "fighter_dob",
-            'opponent_name',
-            "opponent_kd_differential",
-            "opponent_str_differential",
-            "opponent_td_differential",
-            "opponent_sub_differential",
-            "opponent_winrate",
-            "opponent_winstreak",
-            "opponent_losestreak",
-            "opponent_totalwins",
-            "opponent_totalfights",
-            "opponent_titlefights",
-            "opponent_titlewins",
-            "opponent_age_deviation",
-            "opponent_opp_avg_elo",
-            "opponent_elo",
-            "opponent_dob",
-            "result"
-        ]
+    with open(output_csv_filename, mode="a", newline="") as output_file:
         csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-        csv_writer.writeheader()
         csv_writer.writerow(combined_stats)
 
 
 input_csv_filename = "fighter_stats.csv"
 output_csv_filename = "predict_fights_elo.csv"
-fighter_name = "Sean Strickland"
-opponent_name = "Israel Adesanya"
 
-extract_fighter_stats(
-    input_csv_filename, output_csv_filename, fighter_name, opponent_name
-)
-extract_fighter_stats(
-    input_csv_filename, output_csv_filename, opponent_name, fighter_name
-)
+with open(output_csv_filename, mode="w", newline="") as output_file:
+    fieldnames = [
+        "fighter_name",
+        "fighter_kd_differential",
+        "fighter_str_differential",
+        "fighter_td_differential",
+        "fighter_sub_differential",
+        "fighter_winrate",
+        "fighter_winstreak",
+        "fighter_losestreak",
+        "fighter_totalwins",
+        "fighter_totalfights",
+        "fighter_titlefights",
+        "fighter_titlewins",
+        "fighter_age_deviation",
+        "fighter_opp_avg_elo",
+        "fighter_elo",
+        "fighter_dob",
+        'opponent_name',
+        "opponent_kd_differential",
+        "opponent_str_differential",
+        "opponent_td_differential",
+        "opponent_sub_differential",
+        "opponent_winrate",
+        "opponent_winstreak",
+        "opponent_losestreak",
+        "opponent_totalwins",
+        "opponent_totalfights",
+        "opponent_titlefights",
+        "opponent_titlewins",
+        "opponent_age_deviation",
+        "opponent_opp_avg_elo",
+        "opponent_elo",
+        "opponent_dob",
+        "result"
+    ]
+    csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+fights=[["Sean Strickland", "Israel Adesanya"]]
+for fight in fights:
+    fighter_name = fight[0]
+    opponent_name = fight[1]
+
+    extract_fighter_stats(
+        input_csv_filename, output_csv_filename, fighter_name, opponent_name
+    )
+    extract_fighter_stats(
+        input_csv_filename, output_csv_filename, opponent_name, fighter_name
+    )
