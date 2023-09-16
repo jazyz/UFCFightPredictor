b07fb87b9a721a6d275d5ebde8bf7fa894b54cec
jazyz <johnlikeapple37@gmail.com>
2023-09-16 14:29:17 -0400

    updated training and predictions

--- ml_training_duplication.py
+++ ml_training_duplication.py
@@ -7,12 +7,15 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("elofightstats.csv")
+data['date'] = pd.to_datetime(data['date'], format='%b. %d, %Y')
+data = data.sort_values(by='date')
 data.replace("--", pd.NA, inplace=True)
-data = data[(data['fighter_totalfights'] > 4) & (data['opponent_totalfights'] > 4)]
-# data = data[pd.to_datetime(data["date"]).dt.year>2008]
-data["fighter_dob"] = pd.to_datetime(data["fighter_dob"]).dt.year
-data["opponent_dob"] = pd.to_datetime(data["opponent_dob"]).dt.year
+data = data[(data['fighter_totalfights'] > 2) & (data['opponent_totalfights'] > 2)]
+data = data[pd.to_datetime(data["date"]).dt.year>=2010]
+# data["fighter_dob"] = pd.to_datetime(data["fighter_dob"]).dt.year
+# data["opponent_dob"] = pd.to_datetime(data["opponent_dob"]).dt.year
 selected_columns = [
+    "date",
     "fighter_kd_differential",
     "fighter_str_differential",
     "fighter_td_differential",
@@ -20,8 +23,8 @@ selected_columns = [
     # "fighter_winrate",
     "fighter_winstreak",
     "fighter_losestreak",
-    "fighter_totalfights",
-    "fighter_totalwins",
+    # "fighter_totalfights",
+    # "fighter_totalwins",
     "fighter_age_deviation",
     "fighter_titlefights",
     "fighter_titlewins",
@@ -34,15 +37,15 @@ selected_columns = [
     # "opponent_winrate",
     "opponent_winstreak",
     "opponent_losestreak",
-    "opponent_totalfights",
-    "opponent_totalwins",
+    # "opponent_totalfights",
+    # "opponent_totalwins",
     "opponent_age_deviation",   
     "opponent_titlefights",
     "opponent_titlewins",
     "opponent_elo",
     "opponent_opp_avg_elo",
-    "fighter_dob",
-    "opponent_dob",
+    # "fighter_dob",
+    # "opponent_dob",
     "result",
 ]
 
@@ -60,10 +63,28 @@ X = data.drop("result", axis=1)
 y = data["result"]
 
 seed=42
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=seed
-)
 
+# print(len(X))
+# split_index = int(len(X) * 0.98)
+# print(len(X)-split_index)
+# X_train, X_test = X[:split_index], X[split_index:]
+# y_train, y_test = y[:split_index], y[split_index:]
+
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=42
+# )
+
+train_data = data[data['date'] < '2023-09-01']
+test_data = data[data['date'] >= '2023-09-01']
+
+X_train = train_data.drop(["result","date"], axis=1)
+y_train = train_data["result"]
+
+X_test = test_data.drop(["result","date"], axis=1)
+y_test = test_data["result"]
+
+print(len(X_train))
+print(len(X_test))
 fighter_train = X_train.copy()
 
 # Rename columns
@@ -103,10 +124,10 @@ print(len(combined_y_train))
 model = lgb.LGBMClassifier(random_state=seed)
 model.fit(combined_X_train, combined_y_train)
 
-y_pred = model.predict(X_test)
+# y_pred = model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.4f}")
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f"Accuracy: {accuracy:.4f}")
 
 output_file = open("ml_elo.txt", "w")
 original_stdout = sys.stdout
@@ -116,13 +137,13 @@ pd.set_option("display.max_columns", None)
 predict_data = pd.read_csv("predict_fights_elo.csv")
 predict_data.replace("--", pd.NA, inplace=True)
 
-predict_data["fighter_dob"] = pd.to_datetime(predict_data["fighter_dob"]).dt.year
-predict_data["opponent_dob"] = pd.to_datetime(predict_data["opponent_dob"]).dt.year
+# predict_data["fighter_dob"] = pd.to_datetime(predict_data["fighter_dob"]).dt.year
+# predict_data["opponent_dob"] = pd.to_datetime(predict_data["opponent_dob"]).dt.year
 
 predict_data.dropna(subset=selected_columns, inplace=True)
 predict_data = predict_data[selected_columns]
 
-X_predict = predict_data.drop("result", axis=1)
+X_predict = predict_data.drop(["result","date"], axis=1)
 
 y_pred = model.predict(X_predict)
 
