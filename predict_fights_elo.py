c39989e617b128c59a12415485b4fc00272f9a95
Alex Xu <alex.xu.205@gmail.com>
2023-11-17 13:38:13 -0500

    updated odds for nov 18

--- predict_fights_elo.py
+++ predict_fights_elo.py
@@ -13,7 +13,7 @@ def main():
     response = requests.get(url)
 
     # to get one event, paste the url in event_urls and comment out the part below
-    event_urls = ["http://www.ufcstats.com/event-details/5a558ba1ff5e9121"]
+    event_urls = ["http://ufcstats.com/event-details/3feed7d02711d8b7"]
 
     # if response.status_code == 200:
     #     soup = BeautifulSoup(response.text, 'html.parser')
