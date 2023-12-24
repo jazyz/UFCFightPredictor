b6a6b5fc8bf8ea64cb93072044ffc79ead275f44
jazyz <johnlikeapple37@gmail.com>
2023-12-24 15:11:34 -0500

    model tuning hyperparameters

--- ml_alpha.py
+++ ml_alpha.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import sys
 import lightgbm as lgb
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+from bayes_opt import BayesianOptimization
+from sklearn.model_selection import cross_val_score
 
 # Step 1: Read the data
 df = pd.read_csv("detailed_fights.csv")
@@ -83,7 +85,7 @@ split_index = int(len(df) * 0.9)
 X_train, X_test = X[:split_index], X[split_index:]
 y_train, y_test = y[:split_index], y[split_index:]
 
-seed = 31
+seed = 42
 
 # Determine the new start index for the training data to skip the first 20%
 prune_index = int(len(X_train) * 0.2)
@@ -117,16 +119,30 @@ y_train_swapped = y_train_swapped.apply(lambda x: 2 if x == 1 else (1 if x == 2 
 X_train_extended = pd.concat([X_train, X_train_swapped], ignore_index=True)
 y_train_extended = pd.concat([y_train, y_train_swapped], ignore_index=True)
 
-# Now, X_train_extended and y_train_extended contain the original training data
-# plus a copy with 'Red' and 'Blue' stats swapped and the target variable inversed.
-# Invert the target variable for the swapped data
-# Continue with model training using the extended training set
-model = lgb.LGBMClassifier(random_state=seed)
+
+
+# Fit the model
+best_params = {
+    'learning_rate': 0.01,
+    'min_data_in_leaf': 100,
+    'num_leaves': 31,
+    'reg_alpha': 0.1
+}
+
+# Initialize and train the model
+model = lgb.LGBMClassifier(
+    random_state=seed,
+    learning_rate=best_params['learning_rate'],
+    min_data_in_leaf=best_params['min_data_in_leaf'],
+    num_leaves=best_params['num_leaves'],
+    reg_alpha=best_params['reg_alpha']
+)
+
+# model.fit(X_train_extended, y_train_extended)
+
 model.fit(X_train_extended, y_train_extended)
-
-
+# Make predictions and evaluate the model
 y_pred = model.predict(X_test)
-
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.4f}")
 
@@ -158,3 +174,56 @@ plt.xlabel("Importance")
 plt.ylabel("Feature")
 plt.title("Feature Importance")
 plt.show()
+
+# def lgb_cv(num_leaves, learning_rate, min_data_in_leaf, reg_alpha):
+#     """LGBM Cross Validation Score.
+#     This function will be passed to Bayesian Optimizer.
+#     """
+#     params = {
+#         'num_leaves': int(round(num_leaves)),
+#         'learning_rate': learning_rate,
+#         'min_data_in_leaf': int(round(min_data_in_leaf)),
+#         'reg_alpha': reg_alpha,
+#         'random_state': seed  # ensure reproducibility
+#     }
+    
+#     # Initialize and cross-validate the model
+#     model = lgb.LGBMClassifier(**params)
+#     cv_score = cross_val_score(model, X_train_extended, y_train_extended, cv=5, scoring='accuracy').mean()
+#     return cv_score
+
+# # Define bounds of hyperparameters for Bayesian Optimization
+# pbounds = {
+#     'num_leaves': (20, 60),
+#     'learning_rate': (0.001, 0.1),
+#     'min_data_in_leaf': (20, 200),
+#     'reg_alpha': (0, 1)
+# }
+
+# # Initialize Bayesian Optimization
+# optimizer = BayesianOptimization(
+#     f=lgb_cv,
+#     pbounds=pbounds,
+#     random_state=42,
+# )
+
+# # Optimize
+# optimizer.maximize(
+#     init_points=2,  # number of initializing random points
+#     n_iter=10,  # number of iterations for optimization
+# )
+
+# # Extract the best parameters
+# best_params = optimizer.max['params']
+
+# # Convert the 'num_leaves' and 'min_data_in_leaf' to int since they must be integers
+# best_params['num_leaves'] = int(round(best_params['num_leaves']))
+# best_params['min_data_in_leaf'] = int(round(best_params['min_data_in_leaf']))
+
+# print("Best parameters:", best_params)
+
+# # Now use the best parameters to fit the model
+# model = lgb.LGBMClassifier(
+#     **best_params,
+#     random_state=seed
+# )
