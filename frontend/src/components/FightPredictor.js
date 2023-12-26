02718f9350d57fd903d12ae53c798b33654ac7cd
Alex Xu <alex.xu.205@gmail.com>
2023-12-25 20:18:37 -0500

    frontend for website

---
+++ frontend/src/components/FightPredictor.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const FightPredictor = () => {
+  const [fighterName1, setFighterName1] = useState("");
+  const [fighterName2, setFighterName2] = useState("");
+  const [stats, setStats] = useState(null);
+  const [predictedData, setPredictedData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const baseURL = "http://127.0.0.1:5000/";
+
+  const handlePredictClick = async () => {
+    try {
+      setIsLoading(true);
+      console.log("Predicting fight...");
+      const response = await axios.post(`${baseURL}/predict`, {
+        fighter_name1: fighterName1,
+        fighter_name2: fighterName2,
+      });
+      console.log(response.data.message);
+      const training = await axios.post(`${baseURL}/train`);
+      console.log(training.data.message);
+      const fighterStats = await axios.get(`${baseURL}/get_stats`);
+      console.log(fighterStats.data);
+      setStats(fighterStats.data[0]);
+      console.log(stats);
+      const results = await axios.get(`${baseURL}/get_predicted_data`);
+      setPredictedData(results.data.predicted_data[0]);
+      console.log(results.data.predicted_data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+
+      console.error("Error predicting fight:", error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 shadow-md rounded-lg w-96">
+        <h2 className="text-2xl font-semibold mb-4">UFC Fight Predictor</h2>
+        <div className="mb-4">
+          <input
+            className="w-full border rounded py-2 px-3"
+            type="text"
+            placeholder="Enter Fighter 1 Name"
+            value={fighterName1}
+            onChange={(e) => setFighterName1(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            className="w-full border rounded py-2 px-3"
+            type="text"
+            placeholder="Enter Fighter 2 Name"
+            value={fighterName2}
+            onChange={(e) => setFighterName2(e.target.value)}
+          />
+        </div>
+        <button
+          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+          onClick={handlePredictClick}
+        >
+          {isLoading ? "Gathering Data..." : "Predict"}
+        </button>
+        {predictedData && (
+          <div className="mt-4 space-x-4">
+            <div className="flex space-x-4">
+              <div className="flex flex-col">
+                <h3 className="text-xl font-semibold mb-2">
+                  {stats.fighter_name}
+                </h3>
+                <p>{stats.fighter_record}</p>
+                <p>{stats.fighter_dob}</p>
+                <p>{stats.fighter_height}</p>
+                <p>{stats.fighter_reach}</p>
+              </div>
+              <div className="flex flex-col">
+                <h3 className="text-xl font-semibold mb-2">
+                  {stats.opponent_name}
+                </h3>
+                <p>{stats.opponent_record}</p>
+                <p>{stats.opponent_dob}</p>
+                <p>{stats.opponent_height}</p>
+                <p>{stats.opponent_reach}</p>
+              </div>
+            </div>
+            <div className="mt-4">
+              <p>
+                Probability Win: {predictedData.probability_win.toFixed(2)}%
+              </p>
+              <p>
+                Probability Lose: {predictedData.probability_loss.toFixed(2)}%
+              </p>
+              <p>
+                {stats.fighter_name}{" "}
+                {predictedData.predicted_result === "win"
+                  ? "defeats"
+                  : "loses to"}{" "}
+                {stats.opponent_name}
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FightPredictor;
--- predict_fights_elo.py
+++ predict_fights_elo.py
@@ -7,6 +7,118 @@ import csv
 input_csv_filename = "fighter_stats.csv"
 output_csv_filename = "predict_fights_elo.csv"
 
+fieldnames = [
+    "id",
+    "event",
+    "date",
+    "fighter_name",
+    "fighter_weight",
+    "fighter_height",
+    "fighter_reach",
+    "fighter_dob",
+    "fighter_kd_differential",
+    "fighter_str_differential",
+    "fighter_td_differential",
+    "fighter_sub_differential",
+    "fighter_winrate",
+    "fighter_winstreak",
+    "fighter_losestreak",
+    "fighter_totalfights",
+    "fighter_totalwins",
+    "fighter_record",
+    "fighter_titlefights",
+    "fighter_titlewins",
+    "fighter_age_deviation",
+    "fighter_elo",
+    "fighter_opp_avg_elo",
+    
+    "fighter_kowin",
+    "fighter_koloss",
+    "fighter_subwin",
+    "fighter_subloss",
+    "fighter_udecwin",
+    "fighter_udecloss",
+    "fighter_sdecwin",
+    "fighter_sdecloss",
+    "fighter_mdecwin",
+    "fighter_mdecloss",
+
+    "opponent_name",
+    "opponent_weight",
+    "opponent_height",
+    "opponent_reach",
+    "opponent_dob",
+    "opponent_kd_differential",
+    "opponent_str_differential",
+    "opponent_td_differential",
+    "opponent_sub_differential",
+    "opponent_winrate",
+    "opponent_winstreak",
+    "opponent_losestreak",
+    "opponent_totalfights",
+    "opponent_totalwins",
+    "opponent_record",
+    "opponent_titlefights",
+    "opponent_titlewins",
+    "opponent_age_deviation",
+    "opponent_elo",
+    "opponent_opp_avg_elo",
+    "opponent_kowin",
+    "opponent_koloss",
+    "opponent_subwin",
+    "opponent_subloss",
+    "opponent_udecwin",
+    "opponent_udecloss",
+    "opponent_sdecwin",
+    "opponent_sdecloss",
+    "opponent_mdecwin",
+    "opponent_mdecloss",
+
+    "result",
+    "method",
+    "round",
+    "time",
+]
+
+# GET ALL THE NAMES OF FIGHTERS ON EACH CARD
+def extract_fighter_stats(
+    input_csv_filename, output_csv_filename, fighter_name, opponent_name
+):
+    fighter_stats = None
+    opponent_stats = None
+
+    with open(input_csv_filename, mode="r", newline="") as input_file:
+        csv_reader = csv.DictReader(input_file)
+        for row in csv_reader:
+            if row["name"] == fighter_name:
+                fighter_stats = row
+            elif row["name"] == opponent_name:
+                opponent_stats = row
+
+    if fighter_stats is None or opponent_stats is None:
+        print("Fighter or opponent not found in the CSV.")
+        return
+
+    if int(fighter_stats["totalfights"]) <= 4 or int(opponent_stats["totalfights"]) <= 4:
+        return
+    combined_stats = {}
+    for key, value in fighter_stats.items():
+        if key == "name":
+            combined_stats["fighter_name"] = fighter_name
+        else:
+            combined_stats["fighter_" + key] = value
+
+    for key, value in opponent_stats.items():
+        if key == "name":
+            combined_stats["opponent_name"] = opponent_name
+        else:
+            combined_stats["opponent_" + key] = value
+    combined_stats["result"] = "unknown"
+    combined_stats["date"] = "unknown"
+    with open(output_csv_filename, mode="a", newline="") as output_file:
+        csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+        csv_writer.writerow(combined_stats)
+
 def main():
     # GET LIST OF EVENTS URLS
     url = "http://www.ufcstats.com/statistics/events/completed?page=all"
@@ -30,118 +142,7 @@ def main():
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
-    # GET ALL THE NAMES OF FIGHTERS ON EACH CARD
-    def extract_fighter_stats(
-        input_csv_filename, output_csv_filename, fighter_name, opponent_name
-    ):
-        fighter_stats = None
-        opponent_stats = None
-
-        with open(input_csv_filename, mode="r", newline="") as input_file:
-            csv_reader = csv.DictReader(input_file)
-            for row in csv_reader:
-                if row["name"] == fighter_name:
-                    fighter_stats = row
-                elif row["name"] == opponent_name:
-                    opponent_stats = row
-
-        if fighter_stats is None or opponent_stats is None:
-            print("Fighter or opponent not found in the CSV.")
-            return
-
-        if int(fighter_stats["totalfights"]) <= 4 or int(opponent_stats["totalfights"]) <= 4:
-            return
-        combined_stats = {}
-        for key, value in fighter_stats.items():
-            if key == "name":
-                combined_stats["fighter_name"] = fighter_name
-            else:
-                combined_stats["fighter_" + key] = value
-
-        for key, value in opponent_stats.items():
-            if key == "name":
-                combined_stats["opponent_name"] = opponent_name
-            else:
-                combined_stats["opponent_" + key] = value
-        combined_stats["result"] = "unknown"
-        combined_stats["date"] = "unknown"
-        with open(output_csv_filename, mode="a", newline="") as output_file:
-            csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-            csv_writer.writerow(combined_stats)
-
     with open(output_csv_filename, mode="w", newline="") as output_file:
-        fieldnames = [
-            "id",
-            "event",
-            "date",
-            "fighter_name",
-            "fighter_weight",
-            "fighter_height",
-            "fighter_reach",
-            "fighter_dob",
-            "fighter_kd_differential",
-            "fighter_str_differential",
-            "fighter_td_differential",
-            "fighter_sub_differential",
-            "fighter_winrate",
-            "fighter_winstreak",
-            "fighter_losestreak",
-            "fighter_totalfights",
-            "fighter_totalwins",
-            "fighter_record",
-            "fighter_titlefights",
-            "fighter_titlewins",
-            "fighter_age_deviation",
-            "fighter_elo",
-            "fighter_opp_avg_elo",
-            
-            "fighter_kowin",
-            "fighter_koloss",
-            "fighter_subwin",
-            "fighter_subloss",
-            "fighter_udecwin",
-            "fighter_udecloss",
-            "fighter_sdecwin",
-            "fighter_sdecloss",
-            "fighter_mdecwin",
-            "fighter_mdecloss",
-
-            "opponent_name",
-            "opponent_weight",
-            "opponent_height",
-            "opponent_reach",
-            "opponent_dob",
-            "opponent_kd_differential",
-            "opponent_str_differential",
-            "opponent_td_differential",
-            "opponent_sub_differential",
-            "opponent_winrate",
-            "opponent_winstreak",
-            "opponent_losestreak",
-            "opponent_totalfights",
-            "opponent_totalwins",
-            "opponent_record",
-            "opponent_titlefights",
-            "opponent_titlewins",
-            "opponent_age_deviation",
-            "opponent_elo",
-            "opponent_opp_avg_elo",
-            "opponent_kowin",
-            "opponent_koloss",
-            "opponent_subwin",
-            "opponent_subloss",
-            "opponent_udecwin",
-            "opponent_udecloss",
-            "opponent_sdecwin",
-            "opponent_sdecloss",
-            "opponent_mdecwin",
-            "opponent_mdecloss",
-
-            "result",
-            "method",
-            "round",
-            "time",
-        ]
         csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         csv_writer.writeheader()
 
@@ -166,13 +167,21 @@ def main():
         for fight in fights:
             fighter_name = fight[0]
             opponent_name = fight[1]
+            process(fighter_name, opponent_name)
 
-            extract_fighter_stats(
-                input_csv_filename, output_csv_filename, fighter_name, opponent_name
-            )
-            extract_fighter_stats(
-                input_csv_filename, output_csv_filename, opponent_name, fighter_name
-            )
+
+def process(fighter1_name, fighter2_name):
+    print(f"Processing {fighter1_name} vs {fighter2_name}")
+    with open(output_csv_filename, mode="w", newline="") as output_file:
+        csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+    extract_fighter_stats(
+        input_csv_filename, output_csv_filename, fighter1_name, fighter2_name
+    )
+    extract_fighter_stats(
+        input_csv_filename, output_csv_filename, fighter2_name, fighter1_name
+    )
+
 
 if __name__ == "__main__":
     main()
