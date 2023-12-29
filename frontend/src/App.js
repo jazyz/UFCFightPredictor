7f4e953336f0c36124093b39bf18e1131d2901cd
jiayishen21 <lucasshen21@gmail.com>
2023-12-29 16:34:00 -0500

    enforce selection of valid fighter names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import FightPredictor from "./components/FightPredictor";
 import About from "./components/About";
 import Results from "./components/Results";
 import Testing from "./components/Testing";
+import axios from "axios";
+import { baseURL } from "./constants";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const [nameOptions, setNameOptions] = useState(['']);
+
+  // fetch all fighter names from backend
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/get-fighter-names`)
+      .then((res) => {
+        setNameOptions(res.data.names);
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error("Error fetching fighter names.");
+      })
+  }, [])
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<FightPredictor />} />
+        <Route path="/" element={<FightPredictor nameOptions={nameOptions} />} />
         <Route path="/about" element={<About />} />
         <Route path="/results" element={<Results />} />
         <Route path="/testing" element={<Testing />} />
       </Routes>
+      <ToastContainer />
     </Router>
   );
 };
