618c562963d2345f3764ae394b1a196ecb61b0b8
jazyz <johnlikeapple37@gmail.com>
2023-12-31 15:29:17 -0500

    more testing

--- ml_alpha.py
+++ ml_alpha.py
@@ -20,198 +20,10 @@ df = pd.read_csv("data\detailed_fights.csv")
 label_encoder = LabelEncoder()
 df["Result"] = label_encoder.fit_transform(df["Result"])
 
-selected_columns = [
-    "Result",
-    # "Red Fighter",
-    # "Blue Fighter",
-    # "Title",
-    "Red dob",
-    "Blue dob",
-    "Red totalfights",
-    "Blue totalfights",
-    "Red elo",
-    "Blue elo",
-    "Red losestreak",
-    "Blue losestreak",
-    "Red winstreak",
-    "Blue winstreak",
-    "Red titlewins",
-    "Blue titlewins",
-    "Red oppelo",
-    "Blue oppelo",
-    "Red wins",
-    "Blue wins",
-    "Red losses",
-    "Blue losses",
-    "Red KD",
-    "Blue KD",
-    "Red KD differential",
-    "Blue KD differential",
-    "Red Sig. str.",
-    "Blue Sig. str.",
-    "Red Sig. str. differential",
-    "Blue Sig. str. differential",
-    "Red Total str.",
-    "Blue Total str.",
-    "Red Total str. differential",
-    "Blue Total str. differential",
-    "Red Td",
-    "Blue Td",
-    "Red Td differential",
-    "Blue Td differential",
-    "Red Sub. att",
-    "Blue Sub. att",
-    "Red Sub. att differential",
-    "Blue Sub. att differential",
-    "Red Rev.",
-    "Blue Rev.",
-    "Red Rev. differential",
-    "Blue Rev. differential",
-    "Red Ctrl",
-    "Blue Ctrl",
-    "Red Ctrl differential",
-    "Blue Ctrl differential",
-    "Red Head",
-    "Blue Head",
-    "Red Head differential",
-    "Blue Head differential",
-    "Red Body",
-    "Blue Body",
-    "Red Body differential",
-    "Blue Body differential",
-    "Red Leg",
-    "Blue Leg",
-    "Red Leg differential",
-    "Blue Leg differential",
-    "Red Distance",
-    "Blue Distance",
-    "Red Distance differential",
-    "Blue Distance differential",
-    "Red Clinch",
-    "Blue Clinch",
-    "Red Clinch differential",
-    "Blue Clinch differential",
-    "Red Ground",
-    "Blue Ground",
-    "Red Ground differential",
-    "Blue Ground differential",
-    "Red Sig. str.%",
-    "Blue Sig. str.%",
-    "Red Sig. str.% differential",
-    "Blue Sig. str.% differential",
-    "Red Sig. str.% defense",
-    "Blue Sig. str.% defense",
-    "Red Total str.%",
-    "Blue Total str.%",
-    "Red Total str.% differential",
-    "Blue Total str.% differential",
-    "Red Total str.% defense",
-    "Blue Total str.% defense",
-    "Red Td%",
-    "Blue Td%",
-    "Red Td% differential",
-    "Blue Td% differential",
-    "Red Td% defense",
-    "Blue Td% defense",
-    "Red Head%",
-    "Blue Head%",
-    "Red Head% differential",
-    "Blue Head% differential",
-    "Red Head% defense",
-    "Blue Head% defense",
-    "Red Body%",
-    "Blue Body%",
-    "Red Body% differential",
-    "Blue Body% differential",
-    "Red Body% defense",
-    "Blue Body% defense",
-    "Red Leg%",
-    "Blue Leg%",
-    "Red Leg% differential",
-    "Blue Leg% differential",
-    "Red Leg% defense",
-    "Blue Leg% defense",
-    "Red Distance%",
-    "Blue Distance%",
-    "Red Distance% differential",
-    "Blue Distance% differential",
-    "Red Distance% defense",
-    "Blue Distance% defense",
-    "Red Clinch%",
-    "Blue Clinch%",
-    "Red Clinch% differential",
-    "Blue Clinch% differential",
-    "Red Clinch% defense",
-    "Blue Clinch% defense",
-    "Red Ground%",
-    "Blue Ground%",
-    "Red Ground% differential",
-    "Blue Ground% differential",
-    "Red Ground% defense",
-    "Blue Ground% defense",
-    "dob oppdiff",
-    "totalfights oppdiff",
-    "elo oppdiff",
-    "losestreak oppdiff",
-    "winstreak oppdiff",
-    "titlewins oppdiff",
-    "oppelo oppdiff",
-    "wins oppdiff",
-    "losses oppdiff",
-    "KD oppdiff",
-    "KD differential oppdiff",
-    "Sig. str. oppdiff",
-    "Sig. str. differential oppdiff",
-    "Total str. oppdiff",
-    "Total str. differential oppdiff",
-    "Td oppdiff",
-    "Td differential oppdiff",
-    "Sub. att oppdiff",
-    "Sub. att differential oppdiff",
-    "Rev. oppdiff",
-    "Rev. differential oppdiff",
-    "Ctrl oppdiff",
-    "Ctrl differential oppdiff",
-    "Head oppdiff",
-    "Head differential oppdiff",
-    "Body oppdiff",
-    "Body differential oppdiff",
-    "Leg oppdiff",
-    "Leg differential oppdiff",
-    "Distance oppdiff",
-    "Distance differential oppdiff",
-    "Clinch oppdiff",
-    "Clinch differential oppdiff",
-    "Ground oppdiff",
-    "Ground differential oppdiff",
-    "Sig. str.% oppdiff",
-    "Sig. str.% differential oppdiff",
-    "Sig. str.% defense oppdiff",
-    "Total str.% oppdiff",
-    "Total str.% differential oppdiff",
-    "Total str.% defense oppdiff",
-    "Td% oppdiff",
-    "Td% differential oppdiff",
-    "Td% defense oppdiff",
-    "Head% oppdiff",
-    "Head% differential oppdiff",
-    "Head% defense oppdiff",
-    "Body% oppdiff",
-    "Body% differential oppdiff",
-    "Body% defense oppdiff",
-    "Leg% oppdiff",
-    "Leg% differential oppdiff",
-    "Leg% defense oppdiff",
-    "Distance% oppdiff",
-    "Distance% differential oppdiff",
-    "Distance% defense oppdiff",
-    "Clinch% oppdiff",
-    "Clinch% differential oppdiff",
-    "Clinch% defense oppdiff",
-    "Ground% oppdiff",
-    "Ground% differential oppdiff",
-    "Ground% defense oppdiff",
-]
+selected_columns = df.columns.tolist()
+
+columns_to_remove = ["Red Fighter", "Blue Fighter", "Title"]
+selected_columns = [col for col in selected_columns if col not in columns_to_remove]
 
 corr_matrix = df[selected_columns].corr().abs()
 
@@ -235,7 +47,7 @@ y = df["Result"]
 # X = pd.get_dummies(X)  # This line is optional and depends on your data
 
 # Manual split based on percentage
-split_index = int(len(df) * 0.9)
+split_index = int(len(df) * 0.8)
 last_index = int(len(df) * 1)
 X_train, X_test = X[:split_index], X[split_index:last_index]
 y_train, y_test = y[:split_index], y[split_index:last_index]
@@ -274,120 +86,64 @@ X_train_extended = pd.concat([X_train, X_train_swapped], ignore_index=True)
 y_train_extended = pd.concat([y_train, y_train_swapped], ignore_index=True)
 
 
-# # for accuracy
 def objective(trial):
     # Parameter suggestions by Optuna for tuning
     param = {
-        "objective": "multiclass",  # or 'binary' for binary classification
-        "verbosity": -1,
-        "boosting_type": "gbdt",  # Default boosting type
-        "num_leaves": trial.suggest_int(
-            "num_leaves", 20, 100
-        ),  # more conservative than default
-        "learning_rate": trial.suggest_float(
-            "learning_rate", 0.02, 0.2, log=True
-        ),  # adjusted range for more granular learning rates
-        "min_child_samples": trial.suggest_int(
-            "min_child_samples", 10, 100
-        ),  # adjusted range to prevent overfitting
-        "subsample": trial.suggest_float(
-            "subsample", 0.5, 1.0
-        ),  # subsample ratio of the training instance
-        "colsample_bytree": trial.suggest_float(
-            "colsample_bytree", 0.5, 1.0
-        ),  # subsample ratio of columns when constructing each tree
-        "n_estimators": 100,  # Fixed number of estimators for simplicity
-        "num_class": 3,  # Replace with the actual number of classes in your dataset
+        'objective': 'multiclass',  # or 'binary' for binary classification
+        'metric': 'multi_logloss',
+        'verbosity': -1,
+        'boosting_type': 'gbdt',    # Default boosting type
+        'num_leaves': trial.suggest_int('num_leaves', 20, 100),  # more conservative than default
+        'learning_rate': trial.suggest_float('learning_rate', 0.02, 0.2, log=True),  # adjusted range for more granular learning rates
+        'min_child_samples': trial.suggest_int('min_child_samples', 10, 100),  # adjusted range to prevent overfitting
+        'subsample': trial.suggest_float('subsample', 0.5, 1.0),  # subsample ratio of the training instance
+        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),  # subsample ratio of columns when constructing each tree
+        'n_estimators': 100,  # Fixed number of estimators for simplicity
+        'num_class': 3  # Replace with the actual number of classes in your dataset
     }
 
-    # Splitting data for validation
-    X_train, X_valid, y_train, y_valid = train_test_split(
-        X_train_extended, y_train_extended, test_size=0.2, stratify=y_train_extended
-    )
+    #     # Splitting data for validation
+    # X_train, X_valid, y_train, y_valid = train_test_split(X_train_extended, y_train_extended, test_size=0.2, stratify=y_train_extended)
 
-    # Creating LightGBM datasets
-    dtrain = lgb.Dataset(X_train, label=y_train)
-    dvalid = lgb.Dataset(X_valid, label=y_valid)
+    # # Creating LightGBM datasets
+    # dtrain = lgb.Dataset(X_train, label=y_train)
+    # dvalid = lgb.Dataset(X_valid, label=y_valid)
 
+    # # Training model
+    # model = lgb.train(
+    #     param,
+    #     dtrain,
+    #     valid_sets=[dvalid],
+    #     callbacks=[lgb.early_stopping(stopping_rounds=30)]
+    # )
+
+    # # Making predictions
+    # preds = model.predict(X_valid, num_iteration=model.best_iteration)
+    # logloss = log_loss(y_valid, preds)
+
+    # return logloss
+
+    data = lgb.Dataset(X_train_extended, label=y_train_extended)
     # Training model
-    model = lgb.train(
+    cv_results = lgb.cv(
         param,
-        dtrain,
-        valid_sets=[dvalid],
-        callbacks=[lgb.early_stopping(stopping_rounds=30)],
+        data,
+        num_boost_round=1000,
+        nfold=3,  # Or another number of folds
+        stratified=True,
+        shuffle=True,
+        callbacks=[lgb.early_stopping(stopping_rounds=20)],
     )
 
-    # Making predictions
-    preds = model.predict(X_valid, num_iteration=model.best_iteration)
-    pred_labels = np.argmax(preds, axis=1)  # For multiclass classification
+    print(cv_results.keys())
 
-    # Calculate accuracy
-    accuracy = accuracy_score(y_valid, pred_labels)
+    # Extract the best score
+    best_score = cv_results['valid multi_logloss-mean'][-1]
 
-    # Return negative accuracy for maximization
-    return (
-        accuracy  # Optuna minimizes the objective, so use negative accuracy to maximize
-    )
-
-
-# def objective(trial):
-#     # Parameter suggestions by Optuna for tuning
-#     param = {
-#         'objective': 'multiclass',  # or 'binary' for binary classification
-#         'metric': 'multi_logloss',
-#         'verbosity': -1,
-#         'boosting_type': 'gbdt',    # Default boosting type
-#         'num_leaves': trial.suggest_int('num_leaves', 20, 100),  # more conservative than default
-#         'learning_rate': trial.suggest_float('learning_rate', 0.02, 0.2, log=True),  # adjusted range for more granular learning rates
-#         'min_child_samples': trial.suggest_int('min_child_samples', 10, 100),  # adjusted range to prevent overfitting
-#         'subsample': trial.suggest_float('subsample', 0.5, 1.0),  # subsample ratio of the training instance
-#         'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),  # subsample ratio of columns when constructing each tree
-#         'n_estimators': 100,  # Fixed number of estimators for simplicity
-#         'num_class': 3  # Replace with the actual number of classes in your dataset
-#     }
-
-#         # Splitting data for validation
-#     X_train, X_valid, y_train, y_valid = train_test_split(X_train_extended, y_train_extended, test_size=0.2, stratify=y_train_extended)
-
-#     # Creating LightGBM datasets
-#     dtrain = lgb.Dataset(X_train, label=y_train)
-#     dvalid = lgb.Dataset(X_valid, label=y_valid)
-
-#     # Training model
-#     model = lgb.train(
-#         param,
-#         dtrain,
-#         valid_sets=[dvalid],
-#         callbacks=[lgb.early_stopping(stopping_rounds=30)]
-#     )
-
-#     # Making predictions
-#     preds = model.predict(X_valid, num_iteration=model.best_iteration)
-#     logloss = log_loss(y_valid, preds)
-
-#     return logloss
-
-#     # data = lgb.Dataset(X_train_extended, label=y_train_extended)
-#     # # Training model
-#     # cv_results = lgb.cv(
-#     #     param,
-#     #     data,
-#     #     num_boost_round=1000,
-#     #     nfold=3,  # Or another number of folds
-#     #     stratified=True,
-#     #     shuffle=True,
-#     #     callbacks=[lgb.early_stopping(stopping_rounds=20)],
-#     # )
-
-#     # print(cv_results.keys())
-
-#     # # Extract the best score
-#     # best_score = cv_results['valid multi_logloss-mean'][-1]
-
-#     # return best_score
+    return best_score
 
 # Create the study object with maximization direction
-study = optuna.create_study(direction="maximize")
+study = optuna.create_study(direction="minimize")
 study.optimize(objective, n_trials=10)
 
 # Fetching the best parameters
@@ -474,23 +230,36 @@ plt.show()
 print("Top 10 Important Features:")
 print(feature_importance_df.head(10))
 
+# # for accuracy
 # def objective(trial):
 #     # Parameter suggestions by Optuna for tuning
 #     param = {
-#         'objective': 'multiclass',  # or 'binary' for binary classification
-#         'verbosity': -1,
-#         'boosting_type': 'gbdt',    # Default boosting type
-#         'num_leaves': trial.suggest_int('num_leaves', 30, 150),  # more conservative than default
-#         'learning_rate': trial.suggest_float('learning_rate', 0.02, 0.2, log=True),  # adjusted range for more granular learning rates
-#         'min_child_samples': trial.suggest_int('min_child_samples', 20, 100),  # adjusted range to prevent overfitting
-#         'subsample': trial.suggest_float('subsample', 0.5, 1.0),  # subsample ratio of the training instance
-#         'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),  # subsample ratio of columns when constructing each tree
-#         'n_estimators': 100,  # Fixed number of estimators for simplicity
-#         'num_class': 3  # Replace with the actual number of classes in your dataset
+#         "objective": "multiclass",  # or 'binary' for binary classification
+#         "verbosity": -1,
+#         "boosting_type": "gbdt",  # Default boosting type
+#         "num_leaves": trial.suggest_int(
+#             "num_leaves", 20, 100
+#         ),  # more conservative than default
+#         "learning_rate": trial.suggest_float(
+#             "learning_rate", 0.02, 0.2, log=True
+#         ),  # adjusted range for more granular learning rates
+#         "min_child_samples": trial.suggest_int(
+#             "min_child_samples", 10, 100
+#         ),  # adjusted range to prevent overfitting
+#         "subsample": trial.suggest_float(
+#             "subsample", 0.5, 1.0
+#         ),  # subsample ratio of the training instance
+#         "colsample_bytree": trial.suggest_float(
+#             "colsample_bytree", 0.5, 1.0
+#         ),  # subsample ratio of columns when constructing each tree
+#         "n_estimators": 100,  # Fixed number of estimators for simplicity
+#         "num_class": 3,  # Replace with the actual number of classes in your dataset
 #     }
 
 #     # Splitting data for validation
-#     X_train, X_valid, y_train, y_valid = train_test_split(X_train_extended, y_train_extended, test_size=0.2, stratify=y_train_extended)
+#     X_train, X_valid, y_train, y_valid = train_test_split(
+#         X_train_extended, y_train_extended, test_size=0.2, stratify=y_train_extended
+#     )
 
 #     # Creating LightGBM datasets
 #     dtrain = lgb.Dataset(X_train, label=y_train)
@@ -501,7 +270,7 @@ print(feature_importance_df.head(10))
 #         param,
 #         dtrain,
 #         valid_sets=[dvalid],
-#         callbacks=[lgb.early_stopping(stopping_rounds=10)]
+#         callbacks=[lgb.early_stopping(stopping_rounds=10)],
 #     )
 
 #     # Making predictions
@@ -512,4 +281,6 @@ print(feature_importance_df.head(10))
 #     accuracy = accuracy_score(y_valid, pred_labels)
 
 #     # Return negative accuracy for maximization
-#     return accuracy  # Optuna minimizes the objective, so use negative accuracy to maximize
+#     return (
+#         accuracy  # Optuna minimizes the objective, so use negative accuracy to maximize
+#     )
