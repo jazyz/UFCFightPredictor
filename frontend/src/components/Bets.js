2d7294609b3c9ef9e5bf1767be6635edc251a504
Alex Xu <alex.xu.205@gmail.com>
2024-01-08 00:30:40 -0500

    bets graph

--- frontend/src/components/Bets.js
+++ frontend/src/components/Bets.js
@@ -1,23 +1,43 @@
-import { React, useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import raw from "../constants/predictions.txt";
+import { baseURL } from "../constants";
 
 const Bets = () => {
-  // let predictions = [];
   const [results, setResults] = useState(null);
-  fetch(raw)
-    .then((r) => r.text())
-    .then((text) => {
-      // console.log("text decoded:", text);
-      setResults(text);
-      //   predictions = text.split("\n");
-      //   console.log("predictions:", predictions);
-    });
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const getImg = await axios.get(`${baseURL}/get_predictions_plot`);
+        setImageSrc(`data:image/png;base64,${getImg.data.image}`);
+
+        const response = await fetch(raw);
+        const text = await response.text();
+        setResults(text);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto mt-8">
       <h2 className="text-2xl font-semibold mb-4">Bets from UFC Predictor</h2>
       <p className="text-md mb-4">
         Paper bets placed starting from September 16, 2023.
       </p>
+      {imageSrc && (
+        <div className="mt-4">
+          <img
+            src={imageSrc}
+            alt="Bankroll Plot"
+            className="max-w-full h-auto"
+          />
+        </div>
+      )}
       <pre className="text-wrap">{results}</pre>
     </div>
   );
